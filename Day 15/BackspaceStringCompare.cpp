c326a92aa7a2e1c1f2cebd7ba60f3f73423f9836
Sneha Jain <126079866+JainSneha6@users.noreply.github.com>
2024-07-30 17:17:50 +0530

    Create BackspaceStringCompare.cpp

---
+++ Day 15/BackspaceStringCompare.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        vector <char> s1,t1;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='#'){
+                if(!s1.empty()){
+                    s1.pop_back();
+                }
+            }
+            else{
+                s1.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<t.size();i++){
+            if(t[i]=='#'){
+                if(!t1.empty()){
+                    t1.pop_back();
+                }
+            }
+            else{
+                t1.push_back(t[i]);
+            }
+        }
+        return s1==t1;
+    }
+};
