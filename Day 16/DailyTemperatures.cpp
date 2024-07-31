4786d2e354878f844292468d7613599ddb1b7421
Sneha Jain <126079866+JainSneha6@users.noreply.github.com>
2024-07-31 13:32:51 +0530

    Update DailyTemperatures.cpp

--- Day 16/DailyTemperatures.cpp
+++ Day 16/DailyTemperatures.cpp
@@ -5,7 +5,7 @@ public:
         int n = temperatures.size();
         vector <int> ans(n,0);
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
+            while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
                 st.pop();
             }
             if(!st.empty()){
