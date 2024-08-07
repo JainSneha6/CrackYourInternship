f140e7ec4376c411b9b8743c4c5d5f7a33bf0743
Sneha Jain <126079866+JainSneha6@users.noreply.github.com>
2024-08-07 19:37:16 +0530

    Update PathSumIII.cpp

--- Day 23/PathSumIII.cpp
+++ Day 23/PathSumIII.cpp
@@ -1,30 +1,30 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
+ *     long long val;
  *     TreeNode *left;
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(long long x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(long long x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 private:
-    int path(TreeNode* root, int targetSum, unordered_map <int,int>& m, int currsum){
+    long long path(TreeNode* root, long long targetSum, unordered_map <long long,long long>& m, long long currsum){
         if(root==NULL){
             return 0;
         }
         currsum+=root->val;
-        int num = m[currsum-targetSum];
+        long long num = m[currsum-targetSum];
         m[currsum]+=1;
-        int res = num + path(root->left,targetSum,m,currsum) + path(root->right,targetSum,m,currsum);
+        long long res = num + path(root->left,targetSum,m,currsum) + path(root->right,targetSum,m,currsum);
         m[currsum]-=1;
         return res;
     }
 public:
-    int pathSum(TreeNode* root, int targetSum) {
-        unordered_map <int,int> m;
+    long long pathSum(TreeNode* root, long long targetSum) {
+        unordered_map <long long,long long> m;
         m[0]=1;
         return path(root,targetSum,m,0);
     }
