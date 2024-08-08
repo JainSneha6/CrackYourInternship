7eb03c954e69faebf1049877bc0bc8598b2e0f77
Sneha Jain <126079866+JainSneha6@users.noreply.github.com>
2024-08-08 19:50:52 +0530

    Create RecoverBinarySearchTree.cpp

---
+++ Day 24/RecoverBinarySearchTree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void inorder(TreeNode* root, TreeNode*& first, TreeNode*& middle, TreeNode*& last, TreeNode*& prev){
+        if(root==NULL){
+            return;
+        }
+        inorder(root->left,first,middle,last,prev);
+        if(prev!=NULL && root->val<prev->val){
+            if(first==NULL){
+                first = prev;
+                middle = root;
+            }
+            else if(last==NULL){
+                last = root;
+            }
+        }
+        prev = root;
+        inorder(root->right,first,middle,last,prev);
+    }
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* first = NULL;
+        TreeNode* middle = NULL;
+        TreeNode* last = NULL;
+        TreeNode* prev = new TreeNode(INT_MIN);
+        inorder(root,first,middle,last,prev);
+        if(first && last){
+            swap(first->val,last->val);
+        }
+        else if(first && middle){
+            swap(first->val,middle->val);
+        }
+    }
+};
